b299f516a9d8ca768bb1b7b6077fbb95f37374c7
khakestari <mostafanmz@yahoo.com>
2025-11-06 16:17:10 +0330

    forkify project: load  & render recipes from API, refactor to MVC architecture, Implementing Error and Success Messages and Search

---
+++ 14 Forkify App/src/js/config.js
@@ -0,0 +1,6 @@
+// API key
+// e284f39f-3a6a-458d-a178-cb466d6be613
+
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 30;
+export const RES_PER_PAGE = 10;
---
+++ 14 Forkify App/src/js/controller.js
@@ -0,0 +1,62 @@
+import * as model from "./model.js";
+import recipeView from "./views/recipeView.js";
+
+// import icons from '../img/icons.svg' // parcel 1
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import searchView from "./views/searchView.js";
+import resultsView from "./views/resultsView.js";
+import paginationView from "./views/paginationView.js";
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+    recipeView.renderSpinner();
+    // 1) loading recipe
+    await model.loadRecipe(id);
+    // 2) rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (error) {
+    recipeView.renderError(`${error} 💥💥💥`);
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+    // get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // load search results
+    await model.loadSearchResults(query);
+    // render results
+    resultsView.render(model.getSearchResultsPage());
+    // render pagination buttons
+
+    paginationView.render(model.state.search);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const controlPagination = function (goToPage) {
+  // 1) Render New results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+  
+  // render new pagination buttons
+  paginationView.render(model.state.search);
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+};
+
+init();
